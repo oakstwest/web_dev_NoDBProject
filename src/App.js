9224faf2776a96852a9a9cfa21db31f9d78dbbb4
Chris <oak.st.west@gmail.com>
2018-12-10 20:04:58 -0800

    8:04 P.M. on 12/10/18

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from "axios";
+
 
 class App extends Component {
+constructor() {
+  super();
+  this.state = {
+
+  }
+}
+
+
+
+
+
   render() {
     return (
       <div className="App">
-       
+       <h1>Words Are Fun</h1>
+       <input type="text"/>
+       <input type="text"/>
+       <input type="text"/>
       </div>
     );
   }
